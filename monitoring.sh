48414634f388c11ae8919894f54d65731552e8c2
Jiseok Son <jiseokson@gmail.com>
2022-11-29 12:50:49 +0900

    monitor

--- monitoring.sh
+++ monitoring.sh
@@ -10,6 +10,8 @@ free -m | awk '/Mem/ {printf "#Memory Usage: %d/%dMB (%.2f%%)\n", $3, $2, $3 / $
 
 df -BM -a | awk '/mapper/ {used += $3; total += $4} END{printf "#Disk Usage: %d/%dGb (%d%%)\n", used, total / 1024, used / total * 100}'
 
+mpstat | awk '/all/ {printf "#CPU load: %.1f%%\n", 100 - $13}'
+
 who -b | awk '{printf "#Last boot: %s %s\n", $3, $4}'
 
 printf "#LVM use: "
@@ -25,3 +27,5 @@ printf "#Connections TCP : %d ESTABLISHED\n" "$(ss -tunpl | grep tcp | wc -l)"
 printf "#User log: %d\n" "$(who | wc -l)"
 
 printf "#Network: IP %s (%s)\n" "$(hostname -I)" "$(ip addr | awk '/ether/{print $2}')"
+
+printf "#Sudo : %d cmd\n" "$(journalctl _COMM=sudo | grep COMMAND | wc -l)"
